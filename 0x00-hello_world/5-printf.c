911a2251d6495102bb099bb6c7d00baa1fdaf253
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-10 19:45:37 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                   
-/**                                                                  
- * main - entry                                                       
- * Return:0 finish                                                     
- */                                                                  
-int main(void)                                                       
-{                                                                    
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                     
-        return(0);                                                   
-} 
+#include <stdio.h>
+/**
+* main- prints using puts function
+*Return:0 when finished
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
