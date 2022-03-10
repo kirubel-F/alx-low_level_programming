799d9b8f1e646a36d4f0ff4a9e96c5730676bd37
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-10 19:56:29 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main- prints using puts function
+*Return:0 when finished
+*/
+int main() {
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+    // sizeof evaluates the size of a variable
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of double: %zu bytes\n", sizeof(doubleType));
+    printf("Size of char: %zu byte\n", sizeof(charType));   
+    return 0;
+}
