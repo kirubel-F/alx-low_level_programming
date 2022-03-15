d31632ec8f836a337cba1779017d03825955fb63
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-15 19:49:54 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,22 @@
-
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: takes in a character
+ * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+    _putchar('+');
+		return (1);
+	}
+	elseif (n = 0)
+	{
+    _putchar('0');
+		return (0);
+	}
+  else (n < 0)
+    _putchar('-');
+    return(-1);
+}
