bf8dbc8ed3327916fc91683aa6821463c7614f9f
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-15 19:18:30 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,15 @@
-
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ **/
+int _islower(int c);
+{
+	char c;
+  if( 'a' <= c || c <= 'z' ){
+    _putchar('1');
+  }
+  else{
+    _putchar('0');
+  }
+}
