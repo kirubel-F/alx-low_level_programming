3ab76f8833effbc63bb9d0350ddcb3d040e888f4
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-15 19:52:05 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,17 +6,17 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-    _putchar('+');
+if (n > 0)
+{
+	_putchar('+');
 		return (1);
-	}
-	elseif (n = 0)
-	{
-    _putchar('0');
+}
+elseif (n = 0)
+{
+    	_putchar('0');
 		return (0);
-	}
+}
   else (n < 0)
-    _putchar('-');
-    return(-1);
+   	_putchar('-');
+    		return(-1);
 }
