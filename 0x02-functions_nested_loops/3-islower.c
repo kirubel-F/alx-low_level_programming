f4bc5e5efe655ab80a192a848ddf4ec3793f8b84
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-15 19:20:58 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet in lowercase
- *
- **/
+ * _islower - returns 1 if lowercase alphabet and 0 otherwise
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
+ */
 int _islower(int c)
 {
-	char c;
-  if( 'a' <= c || c <= 'z' ){
-    _putchar('1');
-  }
-  else{
-    _putchar('0');
-  }
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
