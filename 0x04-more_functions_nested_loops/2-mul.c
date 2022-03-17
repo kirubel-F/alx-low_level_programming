b8c44e9e4a473fa1f379a0a4527552a470ba5ac6
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 20:40:09 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,12 @@
+#include "main.h"
+/**
+* _isdigit - function that check if it's a digit
+* @c: takes in a character/digit
+* Return: 1 if digit and 0 if anything else
+*/
+int mul(int a, int b)
+{
 
+	mul = a*b
+    return (mul);
+}
