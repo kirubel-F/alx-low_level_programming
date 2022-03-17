bd42e2daefc89f4a2df290b55c07eb02e3d8e841
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 20:22:52 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isdigit(int c)
+{
 
+
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+{
+	return (0);
+}
