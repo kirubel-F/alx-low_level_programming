e27eee327aea964b824555a8b24ccfe8040865b6
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 21:29:25 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 
 	for (c = 0; c < 10; c++)
 	{
-    if (c != 2 || c != 4){
-     
+		if (c != 2 || c != 4)
+		
+		{
 		_putchar(c + '0');
-    }
-     
+		}
 	}
 
 	_putchar('\n');
