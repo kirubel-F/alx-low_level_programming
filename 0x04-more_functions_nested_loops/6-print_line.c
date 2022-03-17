bdb1a2f911221e5a7a5a5533223d0d8f0c681427
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 22:18:12 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
+void print_line(int n)
+{
+	int n;
+
+  if (n>0)
+  {  
+    _putchar('_'*n)
+    }
+  else{
+   
+    	_putchar('\n');
+  }
+}
