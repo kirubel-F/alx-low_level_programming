3968416733fbc416bdba24ce70bb33161566c0dd
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 21:45:09 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,24 @@
+#include "main.h"
 
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
+ */
+void print_most_numbers(void)
+{
+  int c;
+	int i;
+  
+  for (c = 0;c < 11;c++)
+  {
+	  for (i = 0; i < 15; i++)
+	  {
+      _putchar(i + '0');
+   
+	
+    }
+
+	  _putchar('\n');
+
+  }
+}
