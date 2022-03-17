1a2719e8419652666a42c5578cf6493dff7746fd
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 20:16:15 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,10 +8,10 @@
 int _isupper(int c)
 {
   
-  if (c >= 'A' && c <= 'Z')
+  if (c >= 'A' && c <= 'Z'){
     return (1);
 }
-  else if {(c>='a' && c<='z'){
+  else if {
     
     return (0);
   }  
