9024f5e13315dc663c84bbc44aa6a0528b67f772
kirubel-F <99495459+kirubel-F@users.noreply.github.com>
2022-03-17 20:53:40 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	char c;
+  for (c = '0';c <= '9';c++)
+  {
+    _putchar('c');
+    return (0);
+   }
+}
